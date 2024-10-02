da9510649d4528e2b9484ecf973031cbb1916c45
me <me@example.com>
2024-10-02 20:27:09 +0800

    move formatting to its own function

--- hexdump.c
+++ hexdump.c
@@ -34,12 +34,82 @@ int GetFileSize(FILE *f)
     return size;
 }
 
-int main(int argc, char **argv) {
-    int i = 0, numread, k = 0, counter = 0, fsize = 0, opt = 0;
+void OutputFormatting(unsigned char *buffer, int bufferSize, int skip_first_column, int skip_third_column)
+{
+    int i = 0, k = 0, counter = 0;
+    char ascii[16] = { 0 };
+
+    for(i = 0; i < bufferSize; i++)
+    {
+	if (i % 16 == 0)
+	{
+            if (!skip_first_column)  // Print the offset if not skipped
+                printf("\n%08X: ", i);
+	    else
+                printf("\n         ");  // Print spaces to maintain alignment
+	}
+
+        if(i % 1 == 0) // 1 space between each byte
+            printf(" ");
+        if(i % 8 == 0) // add extra space every 8 bytes
+            printf(" ");
+        printf("%02X", buffer[i]); // if you use lower case 'x' it will display letters in lower case (in hex numbers)
+
+        ascii[k] = buffer[i];
+
+        if (k == 15)
+	{
+            if(!skip_third_column) // Only print the ASCII representation if not skipped
+	    {
+                printf("  ");
+                counter += 16;
+                for (int j = 0; j < 16; j++)
+                    if (isprint(ascii[j])!= 0)
+                        printf("%c", ascii[j]);
+                    else
+                        printf(".");
+	    }
+                k = 0;
+        }
+        else
+            k++;
+    }
+
+    // check difference between printed chars and bufferSize and print the remaining
+    if(counter < bufferSize)
+    {
+        int remaining = bufferSize - counter;
+
+        while(remaining < 16)
+        {
+            printf(" 00");
+            if(remaining == 7)
+                printf(" ");
+            remaining++;
+        }
+
+        remaining = bufferSize - counter;
+
+	if (!skip_third_column) // Only print the ASCII representation if not skipped
+	{
+            printf("  ");
+            for(k = 0; k < remaining; k++)
+            {
+                if(isprint(ascii[k]))
+                    printf("%c", ascii[k]);
+                else
+                    printf(".");
+            }
+	}
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int numread, fsize = 0, opt = 0;
     int skip_first_column = 0, skip_third_column = 0;
     FILE *fp = NULL;
     unsigned char *buf = NULL;
-    char ascii[16] = { 0 };
 
     if(argc < 2)
 	help(argv[0]);
@@ -89,70 +159,7 @@ int main(int argc, char **argv) {
 	}
     }
 
-    for(i = 0; i < numread; i++)
-    {
-        //if(i % 16 == 0 && !skip_first_column) // Only print the offset if not skipped
-	if (i % 16 == 0)
-	{
-            if (!skip_first_column)  // Print the offset if not skipped
-                printf("\n%08X: ", i);
-	    else
-                printf("\n         ");  // Print spaces to maintain alignment
-	}
-
-        if(i % 1 == 0) // 1 space between each byte
-            printf(" ");
-        if(i % 8 == 0) // add extra space every 8 bytes
-            printf(" ");
-        printf("%02X", buf[i]); // if you use lower case 'x' it will display letters in lower case (in hex numbers)
-
-        ascii[k] = buf[i];
-
-        if (k == 15)
-	{
-            if(!skip_third_column) // Only print the ASCII representation if not skipped
-	    {
-                printf("  ");
-                counter += 16;
-                for (int j = 0; j < 16; j++)
-                    if (isprint(ascii[j])!= 0)
-                        printf("%c", ascii[j]);
-                    else
-                        printf(".");
-	    }
-                k = 0;
-        }
-        else
-            k++;
-    }
-
-    // check difference between printed chars and numread and print the remaining
-    if(counter < numread)
-    {
-        int remaining = numread - counter;
-
-        while(remaining < 16)
-        {
-            printf(" 00");
-            if(remaining == 7)
-                printf(" ");
-            remaining++;
-        }
-
-        remaining = numread - counter;
-
-	if (!skip_third_column) // Only print the ASCII representation if not skipped
-	{
-            printf("  ");
-            for(k = 0; k < remaining; k++)
-            {
-                if(isprint(ascii[k]))
-                    printf("%c", ascii[k]);
-                else
-                    printf(".");
-            }
-	}
-    }
+    OutputFormatting(buf, numread, skip_first_column, skip_third_column);
 
     printf("\n");
     free(buf);
